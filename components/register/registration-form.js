2f9db64f2b56afea94631768465d3cb08ceed54d
Zsolt Ardai <zsolt.ardai@itsquad.hu>
2022-08-15 23:31:02 +0200

    adding an api route for registration and a registration page
    + adding a registration form component

---
+++ components/register/registration-form.js
@@ -0,0 +1,84 @@
+import { useRef } from 'react';
+import Button from '../../components/ui/button';
+import { useRouter } from 'next/router';
+import styles from './registration-form.module.css';
+
+export default function RegistrationForm(props) {
+    const router = useRouter();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+    const codeRef = useRef();
+    async function onSubmitHandler(event) {
+        event.preventDefault();
+
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const confirmPassword = confirmPasswordRef.current.value;
+        const code = codeRef.current.value;
+
+        if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email)) {
+            props.notificationCtx.showNotification({
+                status: 'error',
+                title: 'Error',
+                message: 'The provided e-mail address is not valid!'
+            });
+            return;
+        }
+
+        if (!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).test(password)) {
+            props.notificationCtx.showNotification({
+                status: 'error',
+                title: 'Error',
+                message: 'The provided password is not valid!'
+            });
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            props.notificationCtx.showNotification({
+                status: 'error',
+                title: 'Error',
+                message: 'The provided passwords did not match!'
+            });
+            return;
+        }
+
+        const result = await props.onRegister(email, password, code);
+
+        if (!result) return;
+
+        emailRef.current.value = '';
+        passwordRef.current.value = '';
+        confirmPasswordRef.current.value = '';
+        codeRef.current.value = '';
+        await router.replace('/administration/login');
+    }
+    return (
+      <form className={styles.form} onSubmit={onSubmitHandler}>
+          <h1>Registration</h1>
+          <div className={styles.control}>
+            <label htmlFor='email'>E-mail</label>
+            <input type='email' id='email' required ref={emailRef} placeholder='e.g. example@email.com' />
+          </div>
+          <div className={styles.control}>
+              <label htmlFor='password'>Password</label>
+              <input type='password' id='password' required ref={passwordRef}
+                     placeholder='&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;' />
+          </div>
+          <div className={styles.control}>
+              <label htmlFor='confirmPassword'>Confirm password</label>
+              <input type='password' id='confirmPassword' required ref={confirmPasswordRef}
+                     placeholder='&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;' />
+          </div>
+          <div className={styles.control}>
+              <label htmlFor='code'>Code</label>
+              <input type='password' id='code' required ref={codeRef}
+                     placeholder='&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;' />
+          </div>
+          <div className={styles.actions}>
+              <Button>Register</Button>
+          </div>
+      </form>
+    );
+}
---
+++ pages/administration/register.js
@@ -0,0 +1,50 @@
+import { Fragment, useContext } from 'react';
+import RegistrationForm from '../../components/register/registration-form';
+import Notification from '../../components/layout/notification';
+import NotificationContext from '../../store/notification-context';
+import styles from '../../styles/administration/register.module.css';
+
+export default function Register() {
+    const notificationContext = useContext(NotificationContext);
+    const notification = notificationContext.notification;
+    async function registrationHandler(email, password, code) {
+        notificationContext.showNotification({
+            status: 'pending',
+            title: 'Pending',
+            message: 'Your registration has been sent!'
+        });
+        const body = JSON.stringify({ email: email, password: password, code: code });
+        const headers = { 'Content-Type': 'application/json', Accept: 'application/json' };
+        const response = await fetch('/api/auth/register', { method: 'POST', headers: headers, body: body });
+        const data = await response.json();
+        if (!response.ok) {
+            notificationContext.showNotification({
+                status: 'error',
+                title: 'Error',
+                message: data.message
+            });
+            return false;
+        }
+        notificationContext.showNotification({
+            status: 'success',
+            title: 'Success',
+            message: data.message
+        });
+
+        return true;
+    }
+    return (
+      <Fragment>
+        <RegistrationForm notificationCtx={notificationContext} onRegister={registrationHandler} />
+        {
+          notification
+          &&
+          <Notification
+              status={notification.status}
+              title={notification.title}
+              message={notification.message}
+          />
+        }
+      </Fragment>
+    );
+}
