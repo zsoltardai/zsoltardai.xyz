4cb4f0d50341c34626b047984ac1288677e4f53e
Zsolt Ardai <zsolt.ardai@itsquad.hu>
2022-08-19 19:48:26 +0200

    adding cookie based mode
    from now, it will decide about the current mode based on the cookie which can be set by toggling between dark and light mode

--- store/mode-context.js
+++ store/mode-context.js
@@ -1,13 +1,24 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import { getCookie, setCookie } from 'cookies-next';
 
 const ModeContext = createContext({ mode: 'light', toggleMode: () => {} });
 
 export function ModeContextProvider({ children }) {
     const hour = new Date().getHours();
-
     const [mode, setMode] = useState((hour > 17 || hour < 6) ? 'dark' : 'light');
 
-    const toggleModeHandler = () => setMode((mode === 'light') ? 'dark' : 'light');
+    useEffect(() => {
+        if (getCookie('mode') !== undefined) {
+            const _mode = getCookie('mode');
+            setMode(_ => { return _mode });
+        }
+    }, []);
+
+    const toggleModeHandler = () => {
+        const _mode = (mode === 'light') ? 'dark' : 'light'
+        setMode(_mode);
+        setCookie('mode', _mode);
+    };
 
     const context = { mode: mode, toggleMode: toggleModeHandler };
 
