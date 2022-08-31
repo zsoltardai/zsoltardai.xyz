c259849eb120d0eb5ede363fccc245b672259c11
Zsolt Ardai <zsolt.ardai@itsquad.hu>
2022-08-31 12:43:23 +0200

    opening social pages in new tab

--- components/home/socials.js
+++ components/home/socials.js
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import Instagram from '../icons/instagram';
 import Facebook from '../icons/facebook';
 import LinkedIn from '../icons/linkedin';
@@ -6,18 +5,18 @@ import styles from './socials.module.css';
 
 export default function Socials() {
     return (
-        <Fragment>
+        <>
             <div className={styles.container}>
-                <a href='https://www.facebook.com/zsolt.ardai2000/'>
+                <a href='https://www.facebook.com/zsolt.ardai2000/' target='_blank' rel='noreferrer'>
                     <Facebook width={20} height={20} color='#027BCE' />
                 </a>
-                <a href='https://www.instagram.com/ardai.zsolt/'>
+                <a href='https://www.instagram.com/ardai.zsolt/' target='_blank' rel='noreferrer'>
                     <Instagram width={20} height={20} color='#027BCE' />
                 </a>
-                <a href='https://www.linkedin.com/in/zsoltardai/'>
+                <a href='https://www.linkedin.com/in/zsoltardai/' target='_blank' rel='noreferrer'>
                     <LinkedIn width={20} height={20} color='#027BCE' />
                 </a>
             </div>
-        </Fragment>
+        </>
     );
 }
