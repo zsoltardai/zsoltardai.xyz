f607fb8bb1eba7a26bbd35ce3e49b6c5c9ec76e7
Zsolt Ardai <zsolt.ardai@itsquad.hu>
2022-10-02 18:47:42 +0200

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,62 +1,63 @@
-import { v4 as uuid } from 'uuid';
-import { connect, insert } from '../../lib/db-util';
+import connect from "../../lib/db/connect";
+import crypto from "crypto";
 
 export default async function handler(req, res) {
+    let client; let msg;
     if (req.method === 'POST') {
 
         const { email, firstName, lastName, message } = req.body;
 
-        console.log()
-
         if (!email || !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email)) {
-            res.status(400).json({ message: 'The request did not contain a e-mail address!' });
+            msg = 'The request did not contain a e-mail address!';
+            res.status(400).send(msg);
             return;
         }
 
         if (!firstName || firstName.trim() === '') {
-            res.status(400).json({ message: 'The request did not contain a valid first name!' });
+            msg = 'The request did not contain a valid first name!';
+            res.status(400).send(msg);
             return;
         }
 
         if (!lastName || lastName.trim() === '') {
-            res.status(400).json({ message: 'The request did not contain a valid last name!' });
+            msg = 'The request did not contain a valid last name!';
+            res.status(400).send(msg);
             return;
         }
 
         if (!message || message.trim() === '') {
-            res.status(400).json({ message: 'The request did not contain a valid message!' });
+            msg = 'The request did not contain a valid message!';
+            res.status(400).send(msg);
             return;
         }
 
-        let client;
+        const id = crypto.randomBytes(20).toString('hex');
 
-        try {
-            client = await connect();
-        } catch (error) {
-            res.status(500).json({ message: 'Failed to connect to the database!' });
-            return;
-        }
+        client = await connect();
 
         const contact = {
-            _id: uuid(),
-            email: email,
-            firstName: firstName,
-            lastName: lastName,
-            message: message,
+            _id: id,
+            email,
+            firstName,
+            lastName,
+            message,
             date: new Date().toISOString()
         };
 
         try {
-            await insert(client, 'messages', contact);
-            res.status(201).json({ message: 'Successfully sent the message!', contact: contact });
+            await client.db().collection('messages').insertOne(contact);
         } catch (error) {
-            res.status(500).json({ message: 'Failed to insert message to the database!' });
+            msg = 'Failed to insert message to the database!';
+            res.status(500).send(msg);
+            await client.close();
+            return;
         }
 
+        res.status(201).json(contact);
         await client.close();
-
         return;
     }
 
-    res.status(400).json({ message: 'Only POST requests are allowed!' });
+    msg = 'Only POST requests are allowed!';
+    res.status(400).json(msg);
 }
