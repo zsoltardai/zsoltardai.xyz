19c6adc8c65e526427b7b83951ba6a40ababe3f4
Zsolt Ardai <zsolt.ardai@itsquad.hu>
2023-01-15 21:34:29 +0100

    updating the education section

--- components/about/education.js
+++ components/about/education.js
@@ -2,20 +2,16 @@ import { LinearProgress } from '@mui/material';
 import styles from './education.module.css';
 
 export default function Education() {
-    const total = 210; const current = 182;
+    const total = 210; const current = 210;
     let percentage  = (current/total) * 100;
     return (
       <div className={styles.container}>
           <section>
-              I&apos;m currently studying Business IT at the university of Debrecen.
-              According to my plans I want to finish my Bsc studies in 2023.
-          </section>
-          <section>
-              I still have { total - current } credits left to finish my major, and I cannot wait
-              to hold my degree in my hands.
+              I&apos;ve just finished my Business Informatics (Bsc) major at the University of Debrecen,
+              and I&apos;ll get my degree on hand on the 27th of January.
           </section>
           <LinearProgress className={styles.progress} variant="determinate" value={percentage}  />
-          <p className={styles.center}>{ current } out of { total }</p>
+          <p className={styles.center}>{ current } out of { total } credits</p>
       </div>
     );
 }
